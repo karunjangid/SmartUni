245348a6b2200a738d9db5117de574353b2dfaf9
karunjangid <jangidkind@gmail.com>
2025-03-26 13:34:10 +0530

    na

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
-export default {
+export default defineConfig({
   build: {
-    outDir: 'dist', // Ensure this is correct
+    outDir: 'dist',
   },
-};
-
+});
